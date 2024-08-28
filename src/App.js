46432228460825c05e04e0d71f8194a8f897f97d
hannakfj <hanna.katle.fjon@gmail.com>
2024-08-28 22:01:20 +0200

    fix routing in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'; 
-import About from './Pages/About/About';
-import Referral from './Pages/Referral/Referral';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'; 
 import Treatments from './Pages/Treatments/Treatments';
 import Header from './Components/Header';
 import Contact from './Pages/Contact/Contact';
 import Employees from './Pages/Employees/Employees';
+import About from './Pages/About/About';
+import Referral from './Pages/Referral/Referral';
+import AppointmentLetter from './Pages/AppointmentLetter/AppointmentLetter';
+import TreatmentDetail from './Pages/Treatments/TreatmentDetail';
+
+
+function MainPage() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state && location.state.scrollToId) {
+      const element = document.getElementById(location.state.scrollToId);
+      if (element) {
+        const offset = location.state.offset || 0;
+        const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+        window.scrollTo({
+          top: elementPosition - offset,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }, [location]);
+
+  return (
+    <div id='main'>
+      <About />
+      <Treatments />
+      <Referral />
+      <Employees />
+      <Contact />
+    </div>
+  );
+}
 
 function App() {
   return (
-
     <Router>  
       <div>
         <Header/>
-        <About/>
-        <Treatments/>
-        <Referral/>
-        <Employees/>
-        <Contact/>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/treatment/:treatmentName" element={<TreatmentDetail />} />
+          <Route path="/in" element={<AppointmentLetter />} />
+        </Routes>
       </div>
     </Router>
   );
